eccfda125ddc7a1cd60ab9774a27260b625818d1
mahipal <m@gmail.com>
2018-11-23 01:35:33 +0530

    CF added h

--- src/clubFactory_automate/WriteExcelFile.java
+++ src/clubFactory_automate/WriteExcelFile.java
@@ -1,19 +1,7 @@
 package clubFactory_automate;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.Test;
-
 public class WriteExcelFile {
 
-	@Test
 	public void test(){
 		
 	System.out.println("Selenium Script Excuted");
